71bd50e4b19cf51ffd4d38658794bdd8df443c30
juny429 <juny_0429@codeindesign.kr>
2024-08-20 11:02:47 +0900

    footer 제작

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,12 +1,14 @@
 import { PropsWithChildren } from 'react';
 import Header from '../Header/Header';
 import { commonCss } from '@/styles/common.styles';
+import Footer from '../Footer/Footer';
 
 export default function AppLayout({ children }: PropsWithChildren) {
   return (
     <>
       <Header />
       <main css={commonCss.fullPage}>{children}</main>
+      <Footer />
     </>
   );
 }
