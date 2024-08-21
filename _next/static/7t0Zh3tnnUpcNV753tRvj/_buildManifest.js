5a09846b7139757f93e4e51bdf3a1612fae18e34
juny429 <juny_0429@codeindesign.kr>
2024-08-21 14:07:35 +0900

    Updates

---
+++ _next/static/7t0Zh3tnnUpcNV753tRvj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/83-37494e980bea615b.js","static/css/1d0c805135db8fb9.css","static/chunks/pages/index-2a2c5acdd29176db.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
