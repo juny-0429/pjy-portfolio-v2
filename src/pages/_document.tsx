c1c08d58c583f0a2da1ebd606030f98d8ebee11b
juny429 <juny_0429@codeindesign.kr>
2024-08-23 13:59:43 +0900

    파비콘 안나오는 에러 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head />
+      <link rel="icon" href="/favicon.ico" />
       <body>
         <Main />
         <NextScript />
