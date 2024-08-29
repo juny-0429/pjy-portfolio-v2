cf26a51e23fb104f61fba3e4dfae6b82297b1f9c
juny429 <juny_0429@codeindesign.kr>
2024-08-29 11:45:29 +0900

    progress bar 제작

--- src/features/career/components/MainCareer.styles.ts
+++ src/features/career/components/MainCareer.styles.ts
@@ -1,4 +1,5 @@
 import { commonCss } from '@/styles/common.styles';
+import theme from '@/theme';
 import { css } from '@emotion/react';
 
 export const mainCareerCss = {
@@ -10,5 +11,57 @@ export const mainCareerCss = {
     padding: 74px 0 0;
 
     ${commonCss.sectionMinHeight}
+
+    ${theme.media.pc} {
+      gap: 230px;
+      padding: 120px 0 0;
+    }
+  `,
+
+  title: css`
+    color: ${theme.colors.mainOrange};
+    font-size: 40px;
+    font-weight: 800;
+  `,
+
+  careerList: css`
+    position: relative;
+    width: 100%;
+    height: calc(100dvh - 100px);
+  `,
+};
+
+export const progressBarCss = {
+  backgroundBar: css`
+    position: absolute;
+    top: 0;
+    left: 50%;
+    width: 2px;
+    height: 100%;
+    border-radius: 99px;
+    background: ${theme.colors.mainLightGray};
+    z-index: 1;
+  `,
+
+  activeBar: css`
+    position: absolute;
+    top: 0;
+    left: 50%;
+    width: 2px;
+    height: 100%;
+    border-radius: 99px;
+    background: ${theme.colors.mainGreen};
+    z-index: 2;
+  `,
+
+  dot: css`
+    position: absolute;
+    top: 0;
+    left: calc(50% + 1px);
+    width: 13px;
+    height: 13px;
+    border-radius: 99px;
+    background: ${theme.colors.mainGreen};
+    z-index: 3;
   `,
 };
--- src/features/career/components/MainCareer.tsx
+++ src/features/career/components/MainCareer.tsx
@@ -1,15 +1,58 @@
+import { useRef } from 'react';
 import CareerItem from './CareerItem/CareerItem';
 import CodeInDesignLogo from '@/assets/images/career/code-in-design.png';
 import HimediaLogo from '@/assets/images/career/himedia.png';
 import GreenLogo from '@/assets/images/career/green.png';
-import { mainCareerCss } from './MainCareer.styles';
+import { mainCareerCss, progressBarCss } from './MainCareer.styles';
+import { commonCss } from '@/styles/common.styles';
+import { motion, useScroll, useSpring, useTransform } from 'framer-motion';
 
 export default function MainCareer() {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['start end', 'end start'],
+  });
+
+  const transformedProgress = useTransform(scrollYProgress, [0.2, 0.6], [0, 1]);
+
+  const scaleY = useSpring(transformedProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+
+  const translateY = useTransform(scaleY, [0, 1], ['0%', '100%']);
+
   return (
     <div css={mainCareerCss.wrapper}>
-      <CareerItem title="코드인디자인" logo={CodeInDesignLogo} date="2023.12 ~ 2024.8" description="코드인 디자인은 웹에이전시입니다. 퍼블리싱과 프론트엔드를 맡아서 해왔습니다." />
-      <CareerItem title="React.js 와 Springboot 를 활용한 자바 풀스택 개발자" logo={HimediaLogo} date="2022.10 ~ 2023.3" description="코드인 디자인은 웹에이전시입니다. 퍼블리싱과 프론트엔드를 맡아서 해왔습니다." />
-      <CareerItem title="[웹코딩] UIUX반응형디자인(웹표준,HTML5, CSS)프론트엔드(퍼블리셔)개발자" logo={GreenLogo} date="2022.7 ~ 2022.8" description="코드인 디자인은 웹에이전시입니다. 퍼블리싱과 프론트엔드를 맡아서 해왔습니다." />
+      {/* mobile */}
+      <div css={commonCss.onlyMobileVisible}>
+        <CareerItem title="React.js 와 Springboot 를 활용한 자바 풀스택 개발자" logo={HimediaLogo} date="2022.10 ~ 2023.3" description="코드인 디자인은 웹에이전시입니다. 퍼블리싱과 프론트엔드를 맡아서 해왔습니다." />
+        <CareerItem title="코드인디자인" logo={CodeInDesignLogo} date="2023.12 ~ 2024.8" description="코드인 디자인은 웹에이전시입니다. 퍼블리싱과 프론트엔드를 맡아서 해왔습니다." />
+        <CareerItem title="[웹코딩] UIUX반응형디자인(웹표준,HTML5, CSS)프론트엔드(퍼블리셔)개발자" logo={GreenLogo} date="2022.7 ~ 2022.8" description="코드인 디자인은 웹에이전시입니다. 퍼블리싱과 프론트엔드를 맡아서 해왔습니다." />
+      </div>
+
+      {/* pc */}
+      <p css={[mainCareerCss.title, commonCss.onlyPcVisibleFlex]}>CAREER</p>
+
+      <div css={mainCareerCss.careerList} ref={ref}>
+        <div css={progressBarCss.backgroundBar} />
+        <motion.div
+          css={progressBarCss.activeBar}
+          style={{
+            scaleY,
+            transformOrigin: 'top',
+          }}
+        />
+        <motion.div
+          css={progressBarCss.dot}
+          style={{
+            top: translateY,
+            transform: 'translate(-50%, -50%)',
+          }}
+        />
+      </div>
     </div>
   );
 }
