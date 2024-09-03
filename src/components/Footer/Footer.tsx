2ca7703f4759629c997aa6ef3a4cf89418327467
juny429 <juny_0429@codeindesign.kr>
2024-09-03 14:48:29 +0900

    footer 태그 추가

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,13 +2,13 @@ import { footerCss } from './Footer.styles';
 
 export default function Footer() {
   return (
-    <div css={footerCss.wrapper}>
+    <footer css={footerCss.wrapper}>
       <div css={footerCss.divider} />
 
       <div css={footerCss.copyLightWrapper}>
         <p css={footerCss.copyLight}>{`© 2024 Park JunYoung.\nAll Rights Reserved.`}</p>
         <p css={footerCss.update}>update. 2024/08/09</p>
       </div>
-    </div>
+    </footer>
   );
 }
