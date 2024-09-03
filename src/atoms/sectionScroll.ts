7ea0ced6119de5f9f218d9957a83a13c08140e81
juny429 <juny_0429@codeindesign.kr>
2024-09-03 14:44:55 +0900

    recoil에 프로젝트 메뉴 추가

--- src/atoms/sectionScroll.ts
+++ src/atoms/sectionScroll.ts
@@ -1,7 +1,12 @@
 import { atom } from 'recoil';
-import { MainMenu } from '@/types/menu.types';
+import { MainMenu, ProjectDetailMenu } from '@/types/menu.types';
 
 export const activeSectionState = atom<MainMenu>({
   key: 'activeSectionState',
   default: 'JUNY', // 초기 값 설정
+});
+
+export const activeProjectDetailSectionState = atom<ProjectDetailMenu>({
+  key: 'activeProjectDetailSectionState',
+  default: '프로젝트목적',
 });
