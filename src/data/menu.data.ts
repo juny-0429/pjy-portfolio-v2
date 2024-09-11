cbc31c9c5163c5cc63a6b026449fbea1f9f70b04
juny429 <juny_0429@codeindesign.kr>
2024-09-11 22:36:22 +0900

    커리어 섹션 위치 이동

--- src/data/menu.data.ts
+++ src/data/menu.data.ts
@@ -1,3 +1,3 @@
 import { MainMenu } from '@/types/menu.types';
 
-export const mainMenu: MainMenu[] = ['JUNY', 'ABOUT', 'SKILLS', 'PROJECT', 'CAREER', 'CONNECT'];
+export const mainMenu: MainMenu[] = ['JUNY', 'CAREER', 'ABOUT', 'SKILLS', 'PROJECT', 'CONNECT'];
--- src/types/menu.types.ts
+++ src/types/menu.types.ts
@@ -1,3 +1,3 @@
-export type MainMenu = 'JUNY' | 'ABOUT' | 'SKILLS' | 'PROJECT' | 'CAREER' | 'CONNECT';
+export type MainMenu = 'JUNY' | 'CAREER' | 'ABOUT' | 'SKILLS' | 'PROJECT' | 'CONNECT';
 
 export type ProjectDetailMenu = '프로젝트정보' | '프로젝트소개' | '프로젝트목표' | '나의역할' | '주요이슈및해결과정' | '회고' | '동작구현';
