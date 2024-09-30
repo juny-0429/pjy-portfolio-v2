9499e5213933e21ccf65e0388fb12b04219b61d5
juny429 <juny_0429@codeindesign.kr>
2024-09-30 17:01:35 +0900

    로고 클릭 시 최상단으로 이동

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,6 +6,7 @@ import { activeSectionState } from '@/atoms/sectionScroll';
 import { commonCss, darkModeCss } from '@/styles/common.styles';
 import HeaderMenu from '../HeaderMenu/HeaderMenu';
 import { useDarkMode } from '@/hooks/useDarkMode';
+import { animateScroll } from 'react-scroll';
 
 export default function Header() {
   const [activeSection] = useRecoilState<MainMenu>(activeSectionState);
@@ -14,7 +15,9 @@ export default function Header() {
   return (
     <header css={[headerCss.header, themeMode === 'dark' && darkModeCss.darkModeStyle]}>
       <h1 css={[headerCss.headerTitle, commonCss.onlyMobileVisible]}>{activeSection}</h1>
-      <h1 css={[headerCss.pcHeaderTitle, commonCss.onlyPcVisibleBlock]}>JUNY PORTFOLIO</h1>
+      <button css={[headerCss.pcHeaderTitle, commonCss.onlyPcVisibleBlock]} onClick={() => animateScroll.scrollToTop()}>
+        JUNY PORTFOLIO
+      </button>
 
       <MenuDrawer cssStyle={commonCss.onlyMobileVisible} />
       <HeaderMenu cssStyle={commonCss.onlyPcVisibleFlex} />
