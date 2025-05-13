b2d980f82420ba4bc2c88d0381538dd164108180
juny-0429 <juny_0429@naver.com>
2025-05-14 06:46:52 +0900

    refactor: 프로젝트 데이터 수정

--- src/features/project/data/projectDetail.data.ts
+++ src/features/project/data/projectDetail.data.ts
@@ -4,7 +4,7 @@ export const quickMenuList: { label_en: string; label_ko: string; value: Project
   { label_en: 'Project Information', label_ko: '프로젝트 정보', value: '프로젝트정보' },
   { label_en: 'Project Goal', label_ko: '프로젝트 목표', value: '프로젝트목표' },
   { label_en: 'My Role', label_ko: '나의 역할', value: '나의역할' },
-  { label_en: 'Major Issues and Solutions', label_ko: '주요 이슈 및 해결 과정', value: '이슈및해결과정' },
+  { label_en: 'Major Issues and Solutions', label_ko: '이슈 및 해결 과정', value: '이슈및해결과정' },
   { label_en: 'Retrospective', label_ko: '회고', value: '회고' },
   { label_en: 'Implementation', label_ko: '동작 구현', value: '동작구현' },
 ];
