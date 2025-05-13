f534905e1b3521cf085903027e76620009e8f03b
juny-0429 <juny_0429@naver.com>
2025-05-14 06:35:42 +0900

    refactor: footer - last update 날짜 수정

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
 
       <div css={footerCss.copyLightWrapper}>
         <p css={footerCss.copyLight}>{`© ${new Date().getFullYear()} Park JunYoung.\nAll Rights Reserved.`}</p>
-        <p css={footerCss.update}>last update. 2025/01/20</p>
+        <p css={footerCss.update}>last update. 2025/05/14</p>
       </div>
     </footer>
   );
